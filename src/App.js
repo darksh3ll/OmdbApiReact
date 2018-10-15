4f1ab2736ba56f246b18b12046cdf5ad8b83b5e9
stephane job <darksh3ll.js@gmail.com>
2018-10-16 00:25:56 +0200

    input et recherche ok

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from "./components/Movie/Movie";
-import data from "./components/data/data"
 
 
 
 class App extends Component {
     state = {
         film:[],
+        value:""
+    };
+    fetchData = async () => {
+        const film = this.state.value;
+        await fetch(`http://www.omdbapi.com/?s="${film}"&apikey=4b10f1ee`)
+            .then( response => response.json())
+            .then(response => this.setState({film:response.Search}))
     };
 
+    start =() => {
+        this.fetchData()
+    };
 
-    componentDidMount() {
-        const film = "underworld";
-        fetch(`http://www.omdbapi.com/?s="${film}"&apikey=4b10f1ee`)
-            .then(response => response.json())
-            .then(response => this.setState({film:response.Search}))
-    }
+    search = (e)=> {
+        this.setState({value:e.target.value});
+};
 
     render() {
-        console.log(this.state.film);
-
         return (
       <div className="App">
+          <div className="nav">
+              <button onClick={()=>this.start()}>GO</button>
+              <div>
+                  <input
+                      className="input"
+                      type="text"
+                      placeholder="Search movies"
+                      value={this.state.value}
+                      onChange={e =>this.search(e)}
+                  />
+              </div>
+          </div>
 
+            <div className="cards">
               {
                 this.state.film.map((movie,key) => {
                     return (
@@ -33,11 +50,11 @@ class App extends Component {
                                 Title = {movie.Title}
                                 Year={movie.Year}
                             />
-                        )
+                            )
                 })
               }
-
-          </div>
+            </div>
+      </div>
 
 
     );
